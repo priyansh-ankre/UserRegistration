c245457aea86850d2296a350f5de44fba69a2a78
priyansh-ankre <bod123priyansh@gmail.com>
2020-07-22 18:32:55 +0530

    [Priyansh]Added:validation for email using regex

--- UserRegistrationApp/UserRegistration.cs
+++ UserRegistrationApp/UserRegistration.cs
@@ -33,5 +33,22 @@ namespace UserRegistrationApp
             }
         }
 
+        public static string EMAIL_REGEX1 = "^[a-z0-9]+([._+-][0-9a-z]+)@+[a-z0-9]+.[a-z]{2,4}([.][a-z]{2}$)";
+        public static string EMAIL_REGEX2 = "^[a-z0-9]+([._+-][0-9a-z]+)@+[a-z0-9]+.[a-z]{2,4}$";
+        public static string EMAIL_REGEX3 = "(^[a-z0-9]+)@+[a-z0-9]+.[a-z]{2,4}([.][a-z]{2}$)";
+        public static string EMAIL_REGEX4 = "(^[a-z0-9]+)@+[a-z0-9]+.[a-z]{2,4}$";
+
+        public bool validateEmail(string email)
+        {
+            if(Regex.IsMatch(email,EMAIL_REGEX1) || Regex.IsMatch(email, EMAIL_REGEX2) || Regex.IsMatch(email, EMAIL_REGEX3) || Regex.IsMatch(email, EMAIL_REGEX4))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
