aed829b9ac9903130c1f48ee30147bd685697b63
priyansh-ankre <bod123priyansh@gmail.com>
2020-07-23 15:24:16 +0530

    [Priyansh]Refactored:used proper project structure

---
+++ FellowShipDay2/UserRegistration/UserRegistrationUtility.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    public class UserRegistrationUtility
+    {
+        public static string FIRST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public bool validFirstName(string fname)
+        {
+            if (Regex.IsMatch(fname, FIRST_NAME_REGEX))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public bool validLastName(string lname)
+        {
+            if (Regex.IsMatch(lname, LAST_NAME_REGEX))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string EMAIL_REGEX1 = "^[a-z0-9]+([._+-][0-9a-z]+)@+[a-z0-9]+.[a-z]{2,4}([.][a-z]{2}$)";
+        public static string EMAIL_REGEX2 = "^[a-z0-9]+([._+-][0-9a-z]+)@+[a-z0-9]+.[a-z]{2,4}$";
+        public static string EMAIL_REGEX3 = "(^[a-z0-9]+)@+[a-z0-9]+.[a-z]{2,4}([.][a-z]{2}$)";
+        public static string EMAIL_REGEX4 = "(^[a-z0-9]+)@+[a-z0-9]+.[a-z]{2,4}$";
+
+        public bool validateEmail(string email)
+        {
+            if (Regex.IsMatch(email, EMAIL_REGEX1) || Regex.IsMatch(email, EMAIL_REGEX2) || Regex.IsMatch(email, EMAIL_REGEX3) || Regex.IsMatch(email, EMAIL_REGEX4))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string MOBILE_NO_REGEX = "^+[1-9]{2}[: :][0-9]{10}$";
+
+        public bool validateMobileNo(string mobNo)
+        {
+            if (Regex.IsMatch(mobNo, MOBILE_NO_REGEX))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string PASSWORD_REGEX = "^([a-zA-Z0-9])*[!@#$%^&*]{1}([a-zA-Z0-9])*$";
+
+        public bool validatePassword(string pass)
+        {
+            if (Regex.IsMatch(pass, PASSWORD_REGEX))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ FellowShipDay2/UserRegistrationTest/UserRegistrationUnitTest.cs
@@ -0,0 +1,240 @@
+using NUnit.Framework;
+
+namespace UserRegistrationTest
+{
+    public class UserRegistrationUnitTest
+    {
+
+        [Test]
+        public void givenFirstName_WhenProper_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validFirstName("Priyansh");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenFirstName_WhenNotProper_ShouldReturnFalse()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validFirstName("priyansh");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenLasttName_WhenProper_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validLastName("Ankre");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenLastName_WhenNotProper_ShouldReturnFalse()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validLastName("ankre");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenProper_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc@yahoo.com");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenProperWithOptionPart_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc-100@yahoo.com");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc123@.com");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenMobileNo_WhenProper_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateMobileNo("91 4456830064");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenPassword_WhenProperForLengthUpperCaseNumericValueSpecialCharacter_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validatePassword("abcdE$fgh9");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenPassword_WhenNotProperForLength_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validatePassword("dcGv4");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenPassword_WhenNotProperForUpperCase_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validatePassword("dcgv4abce");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenPassword_WhenNotProperForNumericValue_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validatePassword("dcgvWabce");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenProper1_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc@yahoo.com");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenProper2_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc-100@yahoo.com");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenProper3_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc.100@yahoo.com");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenProper4_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc111@yahoo.com");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenProper5_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc.100@abc.com.au");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenProper6_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc@1.com");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenProper7_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc@gmail.com.co");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenProper8_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc+100@gmail.com");
+            Assert.IsTrue(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper1_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper2_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc@.com.my");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper3_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc123@gmail.a");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper4_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc123@.com");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper5_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc123@.com.com");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper6_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail(".abc@abc.com");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper7_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc()*@gmail.com");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper8_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc@%*.com");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper9_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc..2002@gmail.com");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper10_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc.@gmail.com");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper11_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc@abc@gmail.com");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper12_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc@gmail.com.1a");
+            Assert.IsFalse(isValid);
+        }
+        [Test]
+        public void givenEmail_WhenNotProper13_ShouldReturnTrue()
+        {
+            UserRegistration.UserRegistrationUtility user = new UserRegistration.UserRegistrationUtility();
+            bool isValid = user.validateEmail("abc@gmail.com.aa.au");
+            Assert.IsFalse(isValid);
+        }
+    }
+}
